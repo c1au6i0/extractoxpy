8340cd65dcadb77a93fe99845a20fea276805590
c1au6i0 <claudio.zanettini@gmail.com>
2025-01-14 23:27:59 -0500

    chorse: folder reorg

--- src/extractoxpy/__init__.py
+++ src/extractoxpy/__init__.py
@@ -1,4 +1,30 @@
 # SPDX-FileCopyrightText: 2025-present c1au6i0 <claudio.zanettini@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-from .random_function import get_random_number
+
+# Taken from pandas -----
+# Let users know if they're missing any of our hard dependencies
+_hard_dependencies = ("requests", "rich", "pandas")
+_missing_dependencies = []
+
+for _dependency in _hard_dependencies:
+    try:
+        __import__(_dependency)
+    except ImportError as _e:  # pragma: no cover
+        _missing_dependencies.append(f"{_dependency}: {_e}")
+
+if _missing_dependencies:  # pragma: no cover
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(_missing_dependencies)
+    )
+del _hard_dependencies, _dependency, _missing_dependencies
+
+
+# from pandas._config import (
+#    get_option,
+#    set_option,
+#    reset_option,
+#   describe_option,
+#    option_context,
+#   options,
+# )
